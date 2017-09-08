3e8b652c4228b3743c3914fb5d9016e137b0886a
Jee Roen <georoen@users.noreply.github.com>
2017-09-08 15:52:21 +0200

    Softcoded predictions output

--- examples/detector.c
+++ examples/detector.c
@@ -1,4 +1,6 @@
 #include "darknet.h"
+#include <libgen.h>
+
 
 static int coco_ids[] = {1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90};
 
@@ -628,7 +630,7 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
             save_image(im, outfile);
         }
         else{
-            save_image(im, "predictions");
+            save_image(im, basename( input));
 #ifdef OPENCV
             cvNamedWindow("predictions", CV_WINDOW_NORMAL); 
             if(fullscreen){
